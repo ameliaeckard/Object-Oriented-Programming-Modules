52e7fa91fb372cba516737360af63c30695fd595
Amelia Eckard <120612539+ameliaeckard@users.noreply.github.com>
2025-07-11 22:43:16 -0400

    Updated Notes

---
+++ module1/code/RectangleArea.java
@@ -0,0 +1,10 @@
+public class RectangleArea {
+
+    public static void main(String[] args) {
+        double length = 5.5;
+        double width = 3.2;
+        double area = length * width;
+
+        System.out.println("Area of the rectangle is: " + area);
+    }
+}
